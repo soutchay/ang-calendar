17aef592b086b57733a0a8191e013f1db976ded0
soutchay <chungvus@gmail.com>
2015-03-02 20:55:48 -0800

    add moment to get dates

--- public/app/app.js
+++ public/app/app.js
@@ -1,5 +1,6 @@
 angular.module("calendarApp", ['ui.router'])
 .controller("CalendarController", function($scope){
     this.title = "Calendar";
+    this.day = moment();
     console.log("basic angular setup");
 });
